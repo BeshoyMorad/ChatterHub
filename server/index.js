f9fc55cf8a0b8d079df89b4af4da1e9c61cfe8fb
BeshoyMorad <besh0morta@gmail.com>
2023-09-23 19:49:03 +0300

    Handle send messages in backend

--- server/index.js
+++ server/index.js
@@ -44,6 +44,14 @@ io.on("connection", (socket) => {
     callback();
   });
 
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
