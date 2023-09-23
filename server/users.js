6b67a8550725911902d94dd762ab5111c5728673
BeshoyMorad <besh0morta@gmail.com>
2023-09-23 19:45:31 +0300

    Add user functions

---
+++ server/users.js
@@ -0,0 +1,46 @@
+const users = [];
+
+export const addUser = ({ id, name, room }) => {
+  // Clean the data
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // Validate the data
+  if (!name || !room) {
+    return {
+      error: "Username and room are required!",
+    };
+  }
+
+  // Check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.name === name;
+  });
+  if (existingUser) {
+    return {
+      error: "Username is in use!",
+    };
+  }
+
+  // Store user
+  const user = { id, name, room };
+  users.push(user);
+
+  return { user };
+};
+
+export const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+export const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+export const getUsersInRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
