18140fc8cd2c0728331fadac2e3f6c38841ac356
BeshoyMorad <besh0morta@gmail.com>
2023-09-24 01:11:32 +0300

    Handle sending messages frontend

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,29 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import { useNavigate } from "react-router";
 
 let socket;
 
 function Chat({ name, room }) {
-  // TODO: remove this
-  name = "beshoy";
-  room = "logo";
-
-  const endpoint = "localhost:5000";
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const endpoint = "http://localhost:5000";
 
   useEffect(() => {
     socket = io(endpoint);
 
     socket.emit("join", { name, room }, (error) => {
-      console.log(error);
+      if (error) {
+        console.log(error);
+      }
     });
 
     return () => {
-      socket.emit("disconnect");
+      socket.emit("userDisconnected");
       socket.off();
     };
   }, [endpoint, name, room]);
 
-  return <div>chat</div>;
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = () => {
+    if (message)
+      socket.emit("sendMessage", message, (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          setMessage("");
+        }
+      });
+  };
+
+  return <div></div>;
 }
 
 export default Chat;
