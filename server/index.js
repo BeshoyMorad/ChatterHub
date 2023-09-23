a90f67747374fa9fb2867668dc50c17ec1a6e7ef
BeshoyMorad <besh0morta@gmail.com>
2023-09-23 16:59:00 +0300

    Enable Cors to server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { Server } from "socket.io";
 import http from "http";
 import router from "./router.js";
@@ -7,9 +8,15 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(router);
+app.use(cors());
 
 io.on("connection", (socket) => {
   console.log("User connected");
