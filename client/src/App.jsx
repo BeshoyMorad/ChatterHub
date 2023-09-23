aadcda3e9def2234d3f9ae8c2536d8bd4e6c4c21
BeshoyMorad <besh0morta@gmail.com>
2023-09-23 16:43:44 +0300

    Create Join component and link it with App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,42 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+
 import Join from "./components/Join";
 import Chat from "./components/Chat";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
-    <Router>
-      <Routes>
-        <Route path={"/"} exact element={<Join />} />
-        <Route path={"/chat"} element={<Chat />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route
+            path={"/"}
+            exact
+            element={
+              <Join
+                name={name}
+                setName={setName}
+                room={room}
+                setRoom={setRoom}
+              />
+            }
+          />
+          <Route path={"/chat"} element={<Chat name={name} room={room} />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Join.jsx
+++ client/src/components/Join.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { Box, Button, Divider, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
-function Join(props) {
-  return <div>Join</div>;
+function Join({ name, setName, room, setRoom }) {
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        p: "1.5rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box>
+        <Typography variant="h2" mb="0.5rem" textAlign="center">
+          Join
+        </Typography>
+
+        <Divider
+          sx={{
+            mb: "1rem",
+          }}
+        />
+
+        <Box component="form" autoComplete="off">
+          <TextField
+            fullWidth
+            label="Name"
+            variant="outlined"
+            sx={{ mb: "1rem" }}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            fullWidth
+            label="Room"
+            variant="outlined"
+            sx={{ mb: "1rem" }}
+            onChange={(e) => setRoom(e.target.value)}
+          />
+
+          <Link
+            onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+            to="/chat"
+          >
+            <Button variant="contained" fullWidth type="submit">
+              Sign In
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default Join;
